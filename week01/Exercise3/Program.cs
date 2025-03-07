68974b0209e3f72cf81a5e99a39b5b044ea2bab4
bauntaai-maamau <bauntaai.maamau@gmail.com>
2025-03-08 11:34:48 +1200

    complete stretch challenge

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,31 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Console.Write("What is the magic number? ");
-        //int magicNumber = int.Parse(Console.ReadLine());
+        string response;
 
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 100);
-
-        int guess = -1;
-
-        while (guess != magicNumber)
+        do
         {
-            Console.Write("What is your guess? ");
-            guess = int.Parse(Console.ReadLine());
+            //Console.Write("What is the magic number? ");
+            //int magicNumber = int.Parse(Console.ReadLine());
 
-            if (magicNumber > guess)
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+
+            int guess = -1;
+            int guessCount = 0;
+
+            while (guess != magicNumber)
             {
-                Console.WriteLine("Higher");
+                Console.Write("Guess a number between 1 & 100. What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it! It took you {guessCount} guesses. ");
+                }
             }
-            else if (magicNumber < guess)
-            {
-                Console.WriteLine("Lower");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it!");
-            }
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            response = Console.ReadLine().ToLower();
+
         }
+
+        while (response == "yes");
+        Console.WriteLine("Thanks for playing! See you next time.");
+
     }
 }
