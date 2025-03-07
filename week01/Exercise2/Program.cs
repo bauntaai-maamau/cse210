cc200e92566bf3f95ac11500c77f1aac56b4d0f6
bauntaai-maamau <bauntaai.maamau@gmail.com>
2025-03-07 21:11:42 +1200

    complete stretch exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -31,6 +31,23 @@ class Program
             letter = "F";
         }
 
+        if (percent >= 97 && percent <= 100 && letter == "A")
+            letter += "+";
+        else if (percent >= 90 && percent < 93 && letter == "A")
+            letter += "-";
+        else if (percent >= 87 && percent < 90 && letter == "B")
+            letter += "+";
+        else if (percent >= 80 && percent < 83 && letter == "B")
+            letter += "-";
+        else if (percent >= 77 && percent < 80 && letter == "C")
+            letter += "+";
+        else if (percent >= 70 && percent < 73 && letter == "C")
+            letter += "-";
+        else if (percent >= 77 && percent < 70 && letter == "D")
+            letter += "+";
+        else if (percent >= 60 && percent < 63 && letter == "D")
+            letter += "-";
+
         Console.WriteLine($"Your letter grade is {letter}");
 
         if (percent >= 70)
