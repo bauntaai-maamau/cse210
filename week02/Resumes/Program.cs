e56d2db38b5ce4ccfb261c6242446d21c1f034bf
bauntaai-maamau <bauntaai.maamau@gmail.com>
2025-03-17 14:58:06 +1200

    add job1 and job2

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Accounting Assistant";
+        job1._company = "KIT";
+        job1._startYear = "2022";
+        job1._endYear = "2024";
+
+        Job job2 = new Job();
+        job2._jobTitle = "Economist";
+        job2._company = "MFED";
+        job2._startYear = 2024;
+        job2._endYear = 2025;
     }
 }
