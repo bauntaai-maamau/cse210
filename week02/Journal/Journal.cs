8ac09a886d2068169ae90cc82786a401dbe5e24d
bauntaai-maamau <bauntaai.maamau@gmail.com>
2025-03-17 19:27:20 +1200

    add journal entries

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -1,8 +1,37 @@
 using System;
-using System.Security.Cryptography.X509Certificates;
 
 public class Journal
 {
+    public List<Entry> entries;
+    public PromptGenerator promptGenerator;
+    public Journal()
+    {
+        entries = new List<Entry>();
+        promptGenerator = new PromptGenerator();
+    }
 
+    //Adds a new entry to the journal from user input
+    public void AddEntry()
+    {
+        string _prompts = promptGenerator.GenerateRandomPrompt();
+        Console.WriteLine($"Today's Prompt: {_prompts}");
+        Console.Write("> ");
+        string content = Console.ReadLine();
+
+        Entry newEntry = new Entry(_prompts, content);
+
+        entries.Add(newEntry);
+
+        newEntry.Display();
+    }
+
+    public void DisplayEntries()
+    {
+        foreach (var entry in entries)
+        {
+            entry.Display();
+        }
+    }
 
 }
+
