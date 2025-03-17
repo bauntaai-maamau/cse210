d111fff871bf52f623fd34dc6cc308ea5aaf7ea4
bauntaai-maamau <bauntaai.maamau@gmail.com>
2025-03-17 22:10:56 +1200

    added program

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -17,11 +17,8 @@ public class Journal
         Console.WriteLine($"Today's Prompt: {_prompts}");
         Console.Write("> ");
         string content = Console.ReadLine();
-
         Entry newEntry = new Entry(_prompts, content);
-
         entries.Add(newEntry);
-
         newEntry.Display();
     }
 
@@ -30,6 +27,44 @@ public class Journal
         foreach (var entry in entries)
         {
             entry.Display();
+        }
+    }
+
+    public void SaveToFile(string fileName)
+    {
+        using (StreamWriter writer = new StreamWriter(fileName, append: true))
+        {
+            foreach (var entry in entries)
+            {
+                writer.WriteLine($"Date: {entry._date}");
+                writer.WriteLine($"Prompt: {entry._prompts}");
+                writer.WriteLine($"Entry: {entry._entryText}");
+                writer.WriteLine();
+            }
+        }
+        Console.WriteLine("Entries save to file.");
+    }
+
+    public void LoadFromFile()
+    {
+        Console.Write("Please enter the file name you would like to load from: ");
+        String filename = Console.ReadLine();
+        try
+        {
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string dateString = reader.ReadLine();
+                    string content = reader.ReadLine();
+                    Entry entry = new Entry(dateString, content);
+                    entries.Add(entry);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error loading entries from :filename: {e.Message}");
         }
     }
 
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,8 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
+        Journal journal = new Journal();
         PromptGenerator promptGenerator = new PromptGenerator();
         string randomPromt = promptGenerator.GenerateRandomPrompt();
-        
+
+        while (true)
+        {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1. Add Entry");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+
+            Console.Write("Please select an option (1-5) ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                //Adds new entry
+                case "1":
+                    journal.AddEntry();
+                    break;
+
+                case "2":
+                    journal.DisplayEntries();
+                    break;
+
+                case "3":
+                    journal.LoadFromFile();
+                    break;
+
+                case "4":
+                    Console.Write("Enter the file name to save: ");
+                    string fileName = Console.ReadLine();
+                    journal.SaveToFile(fileName);  // Pass the file name as an argument
+                    break;
+
+
+                case "5":
+                    Console.WriteLine("Goodbye!");
+                    return;
+
+                default:
+                    Console.WriteLine("Invalid selection, please enter a number 1-5.");
+                    break;
+
+            }
+        }
     }
 }
