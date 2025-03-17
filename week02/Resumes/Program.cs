2976f98d287585630641dcb029233ba4d8d5b59e
bauntaai-maamau <bauntaai.maamau@gmail.com>
2025-03-17 15:11:50 +1200

    add resume to program.cs

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -7,13 +7,21 @@ class Program
         Job job1 = new Job();
         job1._jobTitle = "Accounting Assistant";
         job1._company = "KIT";
-        job1._startYear = "2022";
-        job1._endYear = "2024";
+        job1._startYear = 2022;
+        job1._endYear = 2024;
 
         Job job2 = new Job();
         job2._jobTitle = "Economist";
         job2._company = "MFED";
         job2._startYear = 2024;
         job2._endYear = 2025;
+
+        Resume myResume = new Resume();
+        myResume._name = "Peter Parker";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
