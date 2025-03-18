4a8996f7b14f3e18dccb13e087a66913470a2211
bauntaai-maamau <bauntaai.maamau@gmail.com>
2025-03-18 23:04:35 +1200

    update the program csharp

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,12 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine();
-        Console.WriteLine("Welcome to week 3 project!!");
+        
+        var reference = new Reference("John", 3, 16);
+        var scripture = new Scripture(reference, "For God so love the world that He gave his only begotten Son.");
 
-        Reference b1 = new Reference("John", 14, 15);
-        Console.WriteLine(b1.ScriptReference());
-        Console.WriteLine();
+        while (true)
+        {
+            //display the scripture
+            scripture.DisplayScripture();
+
+            //prompt the user
+            Console.WriteLine("\nPress Enter to hide a word or type 'quit' to exit.");
+            string userInput = Console.ReadLine();
+
+            //exit the program if the user type 'quit'
+            if (userInput.ToLower() == "quit")
+            {
+                break;
+            }
+
+            //hide random word if 'Enter' is pressed
+            if (userInput == "")
+            {
+                bool wordToHide = scripture.HideRandomWord();
+                if (!wordToHide || scripture.AllWordsHidden())
+                {
+                    Console.WriteLine("All words are now hidden. The program will exit.");
+                    break;
+                }
+            }
+        }
 
     }
 }
