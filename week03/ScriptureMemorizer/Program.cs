ac61eb4c3875b10042c1782ec3be2f9ee207e3d9
bauntaai-maamau <bauntaai.maamau@gmail.com>
2025-03-18 23:20:58 +1200

    edit program class

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,16 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        
-        var reference = new Reference("John", 3, 16);
-        var scripture = new Scripture(reference, "For God so love the world that He gave his only begotten Son.");
+        //define reference and scripture
+        Reference reference = new Reference("John", 3, 16);
+        string ScriptureText = "For God so love the world that He gave his only begotten Son.";
 
+        //create a new scripture with the reference and text
+        Scripture scripture = new Scripture(reference, ScriptureText);
+
+        //display the scritpure and prompt the user
         while (true)
         {
-            //display the scripture
+            //display the current scripture
             scripture.DisplayScripture();
 
-            //prompt the user
+            //check if all the words are hidden
+            if (scripture.IsCompletelyHidden())
+            {
+                Console.WriteLine("\nAll words are hidden. Exiting the Program.\n");
+                break;
+            }
+
+            //prompt the user for the next action
             Console.WriteLine("\nPress Enter to hide a word or type 'quit' to exit.");
             string userInput = Console.ReadLine();
 
@@ -26,12 +37,13 @@ class Program
             //hide random word if 'Enter' is pressed
             if (userInput == "")
             {
-                bool wordToHide = scripture.HideRandomWord();
-                if (!wordToHide || scripture.AllWordsHidden())
-                {
-                    Console.WriteLine("All words are now hidden. The program will exit.");
-                    break;
-                }
+                scripture.HideRandomWord();
+                //bool wordToHide = scripture.HideRandomWord();
+                //if (!wordToHide || scripture.AllWordsHidden())
+                //{
+                //    Console.WriteLine("All words are now hidden. The program will exit.");
+                //    break;
+                //}
             }
         }
 
