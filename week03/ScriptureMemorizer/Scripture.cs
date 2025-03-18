484320242c81f0320b020d24a003b77c0aaf2758
bauntaai-maamau <bauntaai.maamau@gmail.com>
2025-03-18 20:42:14 +1200

    update words and scripture classes

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -4,16 +4,42 @@ public class Scripture
 {
     //variables
     private Reference _reference;
-    private List<Word> _wordList;
+    private List<Word> _words;
 
 
     //Constructor
     public Scripture(Reference reference, string scriptureText)
     {
         _reference = reference;
-        //_wordList = scriptureText;
+        _words = scriptureText.Split(" ").Select(word => new Word(word)).ToList();
     }
 
+    public void DisplayScripture()
+    {
+        Console.Clear();
+        //Console.WriteLine(_reference.GetReference());
+        Console.WriteLine(string.Join(" ", _words.Select(w => w.Text)));
+    }
 
+    public bool HideRandomWord()
+    {
+        //get a random word index that is not fully hidden
+        Random random = new Random();
+        var hiddenWords = _words.Where(w => !w.IsHidden).ToList();
 
+        if (!hiddenWords.Any())
+        {
+            return false;
+        }
+
+        //randomly select a word to hide
+        var wordToHide = hiddenWords[random.Next(hiddenWords.Count)];
+        wordToHide.Hide();
+        return true;
+    }
+
+    public bool AllWordsHidden()
+    {
+        return _words.All(w => w.IsHidden);
+    }
 }
--- week03/ScriptureMemorizer/Word.cs
+++ week03/ScriptureMemorizer/Word.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.CompilerServices;
 
 public class Word
 {
@@ -12,5 +13,22 @@ public class Word
     public Word(string text)
     {
         _text = text;
+        _isHidden = false;
     }
+
+
+
+    //Get the word or hide if needed
+    public string Text
+    {
+        get
+        {
+            return _isHidden ? "_____" : _text;
+        }
+    }
+    //Hide the word
+    public void Hide() => _isHidden = true;
+
+    //Check if the word is hidden
+    public bool IsHidden => _isHidden;
 }
