92594c0af66301f333f1dd9b7a7e17663ef11699
bauntaai-maamau <bauntaai.maamau@gmail.com>
2025-03-18 15:58:37 +1200

    scripture class added

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Scripture
+{
+    //variables
+    private Reference _reference;
+   // private List<Word> _wordList;
+
+
+    //Constructor
+    public Scripture(Reference reference, string scriptureText)
+    {
+        _reference = reference;
+       // _wordList = scriptureText;
+    }
+
+
+
+}
