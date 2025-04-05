3e4a9b1f8d21c6f7b376dca69ee9803e0edee244
bauntaai-maamau <bauntaai.maamau@gmail.com>
2025-04-05 16:06:10 +1200

    add my comment to program

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -8,14 +8,22 @@ class Program
         Video myvideo = new Video("How to program in C#", "Peter Parker", 600);
 
         // Add some comments to the video
-        myvideo.AddComment(new Comment("Jane", "Great tutorial! Very helpful."));
-        myvideo.AddComment(new Comment("Alice", "I learned so much, thank you!"));
-        myvideo.AddComment(new Comment("Bob", "Nice explanation, but could use more examples."));
+        Comment myComment1 = new Comment("Jane", "Great tutorial! Very helpful.");
+        Comment myComment2 = new Comment("Alice", "I learned so much, thank you!");
+        Comment myComment3 = new Comment("Bob", "Nice explanation, but could use more examples.");
+
+
+        //Add comment to the video
+        myvideo.AddComment(myComment1);
+        myvideo.AddComment(myComment2);
+        myvideo.AddComment(myComment3);
+
 
         // Display all the comments
         myvideo.DisplayVideoInfo();
 
         Console.WriteLine();
+
     }
 
 }
